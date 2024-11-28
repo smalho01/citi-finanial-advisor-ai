6243e67106b4559926542888f7fa6caf275b410d
Sahil Malhotra <88040167+smalho01@users.noreply.github.com>
2024-11-28 20:52:32 +0000

    regex

--- streamlit_app.py
+++ streamlit_app.py
@@ -106,6 +106,10 @@ st.markdown("""
 def escape_math_symbols(text):
    
     formatted_result = text.replace('$', '&#36;')
+    formatted_result = re.sub(r'^\[\s*(.*?)\s*\]$', r'$$\1$$', formatted_result, flags=re.MULTILINE)
+
+    # Replace brackets for inline equations with $
+    formatted_result = re.sub(r'\[(.*?)\]', r'$\1$', formatted_result)
     return formatted_result
 
 # Show title and description.
